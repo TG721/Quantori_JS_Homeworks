46a35e2d3845561b07a7175372788d534aaf62a4
tengiz <tengiz152@gmail.com>
2023-05-11 23:03:19 +0400

    delete functionality

--- HM5-10/introductiontoreact/src/components/Task/Task.jsx
+++ HM5-10/introductiontoreact/src/components/Task/Task.jsx
@@ -1,14 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import {ReactComponent as Logo} from './images/trash.svg';
 import './Task.css';
 
 export default function Task(props){
+    const [visible, setVisible] = useState(true);
+    
+    const handleDelete = () => {
+        setVisible(false);
+        fetch('http://localhost:3001/tasks/' + props.id, {
+      method: 'DELETE',
+      })
+      .then(response => response.json())
+      .then(() => {
+        setVisible(false);
+      })
+      .catch(error => console.error(error));
+      }
+
     return (
-        <li className="task" id={props.id}>
-        <label className="taskLabel" htmlFor="task">
-            <input type="checkbox" />
-            <span className="title">{props.title}</span>
-        </label>
-        <Logo className="trashButton" alt="icon of bin" />
-        </li>
+        <>
+        {visible && (
+          <li className="task" id={props.id}>
+            <label className="taskLabel" htmlFor="task">
+                <input type="checkbox" />
+                <span className="title">{props.title}</span>
+            </label>
+            <Logo className="trashButton" alt="icon of bin" onClick={handleDelete} />
+          </li>
+        )}
+      </>
     )
+
 }
