4a557afd12fd1fd8bacbc8ba7291e02c6db65bcf
tengiz <tengiz152@gmail.com>
2023-05-13 04:48:53 +0400

    Eliminated code repetition

--- HM5-10/introductiontoreact/src/components/Header/Header.jsx
+++ HM5-10/introductiontoreact/src/components/Header/Header.jsx
@@ -1,26 +1,10 @@
-export default function Header(props){
-    const headerElement = props.importance; //h1 h2 h3 h4
-        switch(headerElement){
-            case "h1": {
-                return <h1 className={props.className}>{props.title}</h1>
-            }
-            case "h2": {
-                return <h2 className={props.className}>{props.title}</h2>
-            }
-            case "h3": {
-                return <h3 className={props.className}>{props.title}</h3>
-            }
-            case "h4": {
-                return <h4 className={props.className}>{props.title}</h4>
-            }
-            case "h5": {
-                return <h5 className={props.className}>{props.title}</h5>
-            }
-            case "h6": {
-                return <h6 className={props.className}>{props.title}</h6>
-            }
-            default: {
-                return <h1 className={props.className}>{props.title}</h1>
-            }
-        }
-}
+import React from 'react';
+
+export default function Header(props) {
+    const { importance = 'h1', className, title } = props;
+    const validHeaders = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    const headerTag = validHeaders.includes(importance) ? importance : 'h1';
+    const header = React.createElement(headerTag, { className }, title);
+    return header;
+  }
+  
