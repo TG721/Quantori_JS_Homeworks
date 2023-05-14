29f498ce0fee949836e7bdfa9e148d7cd98ea3ad
tengiz <tengiz152@gmail.com>
2023-05-14 23:55:44 +0400

    added routes

--- HM5-10/reactrouter/src/App.js
+++ HM5-10/reactrouter/src/App.js
@@ -1,30 +1,17 @@
 import React, { useState } from 'react';
-import Button from './components/Button/Button';
-import Header from './components/Header/Header';
-import Input from './components/Input/Input';
-import Weather from './components/Weather/Weather';
-import ListOfTasks from './components/ListOfTasks/ListOfTasks'
-import Modal from './components/Modal/Modal';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Modal from './components/Modal/Modal';
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [searchText, setSearchText] = useState('');
-
   return (
-    <div className="App">
-      <div className="inLineWrapper">
-      <Header title="To Do List" importance="h1"/>
-      <Weather location="Tbilisi"/>
-      </div>
-      <div className='inLineWrapper'>
-      <Input placeholder="Search Task" className="search input"  value={searchText} onChange={(event) => setSearchText(event.target.value)}/> 
-      <Button title="+ New Task" className="newTaskButton button" onClick={() => setShowModal(true)}/>
-      </div >
-      <ListOfTasks searchText={searchText} />
-      <Modal showModal={showModal} setShowModal={setShowModal} className={`modal ${showModal ? 'shown' : 'hidden'}`} />
-    </div>
-  );
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/tasks" element={<Home />} />
+    <Route path="/Tasks/new" element={<Modal />} />
+  </Routes>
+  )
 }
 
 export default App;
